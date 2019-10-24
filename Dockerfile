d51b93e1a87548f4716da3cb25d9ea71046d6751
Petro Shevchenko <pshevchenko@sequans.com>
2019-10-24 13:56:49 +0300

    The second change of Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,7 @@
 FROM ubuntu:16.04
-ADD . /app
-RUN apt-get update
+COPY ./src /usr/local/lib-coap-cpp/src
+RUN apt-get update -y
 RUN	apt-get install git cmake g++ make -y
 RUN git clone https://github.com/wolfSSL/wolfssl.git
 RUN git clone https://github.com/google/googletest.git
 ENTRYPOINT ["/bin/bash"]
-CMD ["--help"]
--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 docker || apt-get install docker docker.io -y
 sudo dockerd
-sudo docker build -t lib-coap-cpp - < Dockerfile
-sudo docker run -v /var/www:/var/www -p 80:80 -t lib-coap-cpp --name coap-container
+sudo docker build -t lib-coap-cpp .
+sudo docker container create --name coap-container
+sudo docker run -v /var/www:/var/www -p 80:80 -t coap-container
 sudo docker exec -i -t coap-container bash

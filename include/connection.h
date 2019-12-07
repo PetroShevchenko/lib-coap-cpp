2b4477cb8b04c85c9b8b6a3708225f5d5f6abaf5
Petro <shevchenko.p.i@gmail.com>
2019-12-07 21:55:06 +0200

    add wolfSSL submodule

--- include/connection.h
+++ include/connection.h
@@ -5,6 +5,18 @@ namespace coap{
 
 class connection {
 
+protected:
+    int _socket;
+    int _port;
+    std::string _address;
+    std::uint8_t * _buffer;
+    size_t _length;
+
+public:
+	virtual void establish();
+	virtual void disconnect();
+	virtual void send();
+	virtual void receive();
 };
 
 
--- include/endpoint.h
+++ include/endpoint.h
@@ -45,33 +45,5 @@ public:
 	endpoint & operator=(const endpoint &) = delete;
 };
 
-class uriEndpoint : private endpoint {
-
-private:
-	LOG_CREATE(ALL,std::clog);
-
-	uri _uri;
-
-public:
-
-	uriEndpoint (std::string path, endpoint::callback_t callbacks[METHODS_COUNT], std::string attributes[METHODS_COUNT]):
-	endpoint(path, callbacks, attributes), _uri(path)
-	{
-		LOG(DEBUG,"Constructor");
-	}
-
-	~uriEndpoint()
-	{
-		LOG_DELETE;
-	}
-	uriEndpoint(const uriEndpoint &) = delete;
-	uriEndpoint & operator=(const uriEndpoint &) = delete;
-
-	uri & get_uri()
-	{
-		return _uri;
-	}
-};
-
 }//coap
 #endif

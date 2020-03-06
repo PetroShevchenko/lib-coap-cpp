715bad35f0d9a4b8508199ac61026dfc4ab1b78d
Petro Shevchenko <pshevchenko@sequans.com>
2020-03-06 20:21:29 +0200

    Draft blockwise

--- include/blockwise.h
+++ include/blockwise.h
@@ -1,8 +1,10 @@
 #ifndef BLOCKWISE_H
 #define BLOCKWISE_H
+#include <vector>
+#include <cstdint>
 #include "log.h"
 #include "error.h"
-#include <vector>
+#include "packet.h"
 
 namespace coap {
 
@@ -10,11 +12,17 @@ class blockwise {
 private:
 	LOG_CREATE(ALL,std::clog);
 	std::vector<std::uint8_t> _data;
+protected:
+	std::uint32_t _number;
+	std::uint32_t _offset;
+	std::uint16_t _size;
+	bool _more;
 
 public:
 	blockwise();
 	~blockwise();
-
+	virtual bool get_header (packet & pack) = 0;
+	virtual bool set_header (packet & pack) = 0;
 };
 
 
@@ -22,14 +30,16 @@ class block1 : public blockwise {
 private:
 	LOG_CREATE(ALL,std::clog);
 public:
-
+	bool get_header (packet & pack);
+	bool set_header (packet & pack);
 };
 
 class block2: public blockwise {
 private:
 	LOG_CREATE(ALL,std::clog);
 public:
-
+	bool get_header (packet & pack);
+	bool set_header (packet & pack);
 };
 }//coap
 
--- src/blockwise.cc
+++ src/blockwise.cc
@@ -1,1 +1,37 @@
 #include "blockwise.h"
+
+
+
+int
+coap_get_header_block1(void *packet, uint32_t *num, uint8_t *more, uint16_t *size, uint32_t *offset)
+{
+  coap_packet_t *const coap_pkt = (coap_packet_t *) packet;
+
+  if (!IS_OPTION(coap_pkt, COAP_OPTION_BLOCK1)) return 0;
+
+  /* pointers may be NULL to get only specific block parameters */
+  if (num!=NULL) *num = coap_pkt->block1_num;
+  if (more!=NULL) *more = coap_pkt->block1_more;
+  if (size!=NULL) *size = coap_pkt->block1_size;
+  if (offset!=NULL) *offset = coap_pkt->block1_offset;
+
+  return 1;
+}
+
+namespace coap {
+
+bool block1::get_header (packet & pack)
+{
+	size_t optionQuantity;
+	const packet::option_t * optionP = pack.find_options(BLOCK_1, &optionQuantity);
+	if (optionP == nullptr) {
+		LOG(DEBUG, "There is no BLOCK 1 option in the packet");
+		return false;
+	}
+	//TODO
+}
+
+
+
+
+}//coap

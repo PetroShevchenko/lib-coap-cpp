49b60e98faffc50d0e6c354e544bc39afbe41bc5
Petro Shevchenko <shevchenko.p.i@gmail.com>
2020-03-11 08:26:29 +0200

    CMakeLists.txt edited

--- scripts/cmake/tests.cmake
+++ scripts/cmake/tests.cmake
@@ -8,11 +8,6 @@ set(TEST2_SRC_LIST tests/test_endpoint.cc)
 set(TEST3_SRC_LIST tests/test_logging.cc)
 set(TEST4_SRC_LIST tests/test_uri.cc)
 
-project (${TEST1_PROJECT_NAME})
-project (${TEST2_PROJECT_NAME})
-project (${TEST3_PROJECT_NAME})
-project (${TEST4_PROJECT_NAME})
-
 add_executable(${TEST1_PROJECT_NAME} ${TEST1_SRC_LIST})
 add_executable(${TEST2_PROJECT_NAME} ${TEST2_SRC_LIST})
 add_executable(${TEST3_PROJECT_NAME} ${TEST3_SRC_LIST})
--- src/blockwise.cc
+++ src/blockwise.cc
@@ -1,37 +1,31 @@
 #include "blockwise.h"
 
-
-
-int
-coap_get_header_block1(void *packet, uint32_t *num, uint8_t *more, uint16_t *size, uint32_t *offset)
+#if 0
+int coap_get_header_block1(void *packet, uint32_t *num, uint8_t *more, uint16_t *size, uint32_t *offset)
 {
-  coap_packet_t *const coap_pkt = (coap_packet_t *) packet;
-
-  if (!IS_OPTION(coap_pkt, COAP_OPTION_BLOCK1)) return 0;
-
-  /* pointers may be NULL to get only specific block parameters */
-  if (num!=NULL) *num = coap_pkt->block1_num;
-  if (more!=NULL) *more = coap_pkt->block1_more;
-  if (size!=NULL) *size = coap_pkt->block1_size;
-  if (offset!=NULL) *offset = coap_pkt->block1_offset;
-
-  return 1;
+	coap_packet_t *const coap_pkt = (coap_packet_t *) packet;
+	if (!IS_OPTION(coap_pkt, COAP_OPTION_BLOCK1)) return 0;
+	/* pointers may be NULL to get only specific block parameters */
+	if (num!=NULL) *num = coap_pkt->block1_num;
+  	if (more!=NULL) *more = coap_pkt->block1_more;
+  	if (size!=NULL) *size = coap_pkt->block1_size;
+  	if (offset!=NULL) *offset = coap_pkt->block1_offset;
+  	return 1;
 }
+#endif
 
 namespace coap {
 
 bool block1::get_header (packet & pack)
 {
-	size_t optionQuantity;
+  	size_t optionQuantity;
 	const packet::option_t * optionP = pack.find_options(BLOCK_1, &optionQuantity);
 	if (optionP == nullptr) {
 		LOG(DEBUG, "There is no BLOCK 1 option in the packet");
 		return false;
 	}
+  	//_number = pack.
 	//TODO
 }
-
-
-
 
 }//coap
